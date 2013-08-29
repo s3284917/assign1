a0587c1321c3a8d788c0c7c4ae1b00ca5b2948ab
s3284917 <s3284917@student.rmit.edu.au>
2013-08-29 11:47:11 +1000

    Added more comments to the index page

--- partB/index.php
+++ partB/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 "http://www.w3.org/TR/html401/loose.dtd">
 <?php
+  //use the php file with database data stored as variables
   require "db.php";
   //function to query sql database to populate pulldown  
   function selectDistinct($conn, $tableName, $attrName, $pulldownName, $defaultVal) {
@@ -12,26 +13,29 @@
       showerror();
  
     print "\n<select name=\"{$pulldownName}\">";
-
+    //Loop through while there are still values in the array
     while ($row = @ mysql_fetch_array($resultId))
     {
       $result = $row[$attrName];
-    
+      //If the defualtVal shows up in the results from quering the DB
+      //Use as default show value in dropdown
       if (isset($defaultVal) && $result == $defaultVal)
         print "\n\t<option selected value=\"{$result}\">{$result}";
-    
+      //Otherwise add row to dropdown.
       else
         print "\n\t<option value=\"{$result}\">{$result}";
-    
+      //End Options to be added
       print "</option>";
     }
-  
+    //End of Dropdown
     print "\n</select>";
   }
-  
+  //Sets $conn and if it doesnt return true, to say success in connecting
+  //to the DBMS then show error
   if (!($conn = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
     showerror();
   }
+  //if can't connected to the database then show an error
   if (!mysql_select_db(DB_NAME, $conn)) {
     showerror();
   }
@@ -67,6 +71,7 @@ function focusElement(element, errorMsg) {
     <title>Wine Search Page</title>
   </head>
   <body bgcolor="white">
+    <!-- Input form to be sent to query DB -->
     <form action="results.php" method="GET" onsubmit="return validateForm(this);">
     <table id='search' border='0'>
       <tr>
@@ -79,23 +84,35 @@ function focusElement(element, errorMsg) {
       </tr>
       <tr>
       <td>Select a region: </td><td>
+      <!-- Dynamically generate dropdown for region names -->
       <?php selectDistinct($conn, "region", "region_name", "regionName", "All"); ?>
       </td></tr>
       <tr><td>Select Grape variety: </td><td>
+      <!-- Dynamically generate dropdown for grape variety -->
       <?php selectDistinct($conn, "grape_variety", "variety", "grapeVariety", "Blanc"); ?>
       </td></tr>
       <tr><td>Select Year range: </td><td>
+      <!-- Dynamically generate 2 drop downs for wine years, to select
+      min and max for a year range -->
       <?php selectDistinct($conn, "wine", "year", "minYear", ""); ?> To 
       <?php selectDistinct($conn, "wine", "year", "maxYear", "1999"); ?>
       </td>
+      <!-- If an error was returned from the results page
+      then display next to fields -->
       <td><?php if ($errYear) { echo $errYear; }?></td></tr>
       <tr><td>Wine Stock Range: </td>
+      <!-- Text boxes to input a range of stock, max is 999 -->
       <td>Min: <input type="text" name="minStock" value="" maxlength="3" size="3">   
       Max: <input type="text" name="maxStock" value="" maxlength="3" size="3"></td>
+      <!-- If an error was returned from the results page
+      then display next to fields -->
       <td><?php if ($errStock) { echo $errStock; }?></td></tr>
       <tr><td>Select Cost Range: </td>
+      <!-- Text boxes to input a range of price, max is a length of 4 -->
       <td>Min: $<input type="text" name="minCost" value="" maxlength="4" size="4"> 
       Max: $<input type="text" name="maxCost" value="" maxlength="4" size="4"></td>
+      <!-- If an error was returned from the results page
+      then display next to fields -->
       <td><?php if ($errCost) { echo $errCost; }?></td></tr>
       <tr>
       <td><input type="submit" value="Search Wines"></td>
