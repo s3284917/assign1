8a35c1a9067cdc2670dfa9bafbaf08f3d200bb90
s3284917 <s3284917@student.rmit.edu.au>
2013-08-29 04:17:07 +0000

    Added stock and revenue rows, coded added to calculate total sales of a wine_id to get revenue, grouped by wine_id ordered by cost ASC;

--- partB/results.php
+++ partB/results.php
@@ -64,7 +64,9 @@
         "\n\t<th>Year</th>" .
         "\n\t<th>Winery</th>" .
         "\n\t<th>Region</th>" .
-        "\n\t<th>Cost</th>\n</tr>";
+        "\n\t<th>Cost</th>" .
+        "\n\t<th>Stock</th>" .
+        "\n\t<th>Revenue</th>\n</tr>";
      
       while ($row = @mysql_fetch_array($result)) {
         
@@ -73,7 +75,9 @@
           "\n\t<td>{$row["year"]}</td>" .
           "\n\t<td>{$row["winery_name"]}</td>" .
           "\n\t<td>{$row["region_name"]}</td>" .
-          "\n\t<td>{$row["cost"]}</tD>\n</tr>";
+          "\n\t<td>{$row["cost"]}</td>" .
+          "\n\t<td>{$row["on_hand"]}</td>" .
+          "\n\t<td>{$row["revenue"]}</td>\n</tr>";
       }
 
       print "\n</table>";
@@ -91,13 +95,14 @@
     showerror();
   }
 
-  $query = "SELECT wine.wine_id, wine.wine_name, variety, year, winery_name, region_name, cost
-FROM winery, region, wine, grape_variety, wine_variety, inventory 
+  $query = "SELECT wine.wine_id, wine.wine_name, variety, year, winery_name, region_name, cost, on_hand, SUM(items.price) AS revenue
+FROM winery, region, wine, grape_variety, wine_variety, inventory, items
 WHERE winery.region_id = region.region_id 
 AND wine.winery_id = winery.winery_id
 AND wine_variety.wine_id = wine.wine_id
 AND wine_variety.variety_id = grape_variety.variety_id
-AND wine.wine_id = inventory.wine_id"; 
+AND wine.wine_id = inventory.wine_id
+AND items.wine_id = wine.wine_id";
 
   if (isset($_GET['regionName']) && $_GET['regionName'] != 'All')
   {
@@ -122,7 +127,7 @@ AND wine.wine_id = inventory.wine_id";
     $query .= " AND cost >= '{$_GET['minCost']}'" .
       " AND cost <= '{$_GET['maxCost']}'";
   }
-  $query .= " ORDER BY cost ASC";
+  $query .= " GROUP BY wine_id ORDER BY cost ASC";
   validateFormInput();
 
   //$query .= ";"
