dc36e2496e37e4bc02db1153b5cb2c1f3dc25161
s3284917 <s3284917@student.rmit.edu.au>
2013-09-02 03:36:52 +0000

    Modified index search page to use PDO to make the code DBMS independant

--- index.php
+++ index.php
@@ -2,50 +2,43 @@
 "http://www.w3.org/TR/html401/loose.dtd">
 <?php
 
-  function showerror() {
-    die("Error " . mysql_errno() . " : " .mysql_error());
-  }
   //use the php file with database data stored as variables
-  require "db.php";
+  require_once "db.php";
+
   //function to query sql database to populate pulldown  
-  function selectDistinct($conn, $tableName, $attrName, $pulldownName, $defaultVal, $notInDB) {
+  function selectDistinct($tableName, $attrName, $pulldownName, $defaultVal, $notInDB) {
     $defaultWithinResultSet = FALSE;
-    //query string for getting data
-    $distinctQuery = "SELECT DISTINCT {$attrName} FROM {$tableName} ORDER BY {$attrName}";
-    //run Query and make sure it doesnt error
-    if (!($resultId = @ mysql_query ($distinctQuery, $conn)))
-      showerror();
+    
+    try {
+      $dsn = DB_ENGINE .':host='. DB_HOST .';dbname='. DB_NAME;
+      $db = new PDO($dsn, DB_USER, DB_PW);
+      //query string for getting data
+      $query = "SELECT DISTINCT {$attrName} FROM {$tableName} ORDER BY {$attrName}";
  
-    print "\n<select name=\"{$pulldownName}\">";
-    /*Used for dropdown that doesnt have All in the DB but dont want to restrict*/
-    if (isset($defaultVal) && isset($notInDB) && $notInDB == true) {
-      print "\n\t<option selected value=\"{$defaultVal}\">{$defaultVal}";
+      print "\n<select name=\"{$pulldownName}\">";
+      /*Used for dropdown that doesnt have All in the DB but dont want to restrict*/
+      if (isset($defaultVal) && isset($notInDB) && $notInDB == true) {
+        print "\n\t<option selected value=\"{$defaultVal}\">{$defaultVal}";
+      }
+      //Loop through while there are still values in the array
+      foreach ($db->query($query) as $row)
+      {
+        $result = $row[$attrName];
+        //If the defualtVal shows up in the results from quering the DB
+        //Use as default show value in dropdown
+        if (isset($defaultVal) && $result == $defaultVal)
+          print "\n\t<option selected value=\"{$result}\">{$result}";
+        //Otherwise add row to dropdown.
+        else
+          print "\n\t<option value=\"{$result}\">{$result}";
+        //End Options to be added
+        print "</option>";
+      }
+      //End of Dropdown
+      print "\n</select>";
+    } catch(PDOException $e) {
+        echo $e->getMesage();
     }
-    //Loop through while there are still values in the array
-    while ($row = @ mysql_fetch_array($resultId))
-    {
-      $result = $row[$attrName];
-      //If the defualtVal shows up in the results from quering the DB
-      //Use as default show value in dropdown
-      if (isset($defaultVal) && $result == $defaultVal)
-        print "\n\t<option selected value=\"{$result}\">{$result}";
-      //Otherwise add row to dropdown.
-      else
-        print "\n\t<option value=\"{$result}\">{$result}";
-      //End Options to be added
-      print "</option>";
-    }
-    //End of Dropdown
-    print "\n</select>";
-  }
-  //Sets $conn and if it doesnt return true, to say success in connecting
-  //to the DBMS then show error
-  if (!($conn = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
-    showerror();
-  }
-  //if can't connected to the database then show an error
-  if (!mysql_select_db(DB_NAME, $conn)) {
-    showerror();
   }
   //Checks GET for if any error values are set, and 
   //then if so sets a variable for them.
@@ -73,17 +66,17 @@
       <tr>
       <td>Select a region: </td><td>
       <!-- Dynamically generate dropdown for region names -->
-      <?php selectDistinct($conn, "region", "region_name", "regionName", "All"); ?>
+      <?php selectDistinct("region", "region_name", "regionName", "All"); ?>
       </td></tr>
       <tr><td>Select Grape variety: </td><td>
       <!-- Dynamically generate dropdown for grape variety -->
-      <?php selectDistinct($conn, "grape_variety", "variety", "grapeVariety", "All", true); ?>
+      <?php selectDistinct("grape_variety", "variety", "grapeVariety", "All", true); ?>
       </td></tr>
       <tr><td>Select Year range: </td><td>
       <!-- Dynamically generate 2 drop downs for wine years, to select
       min and max for a year range -->
-      <?php selectDistinct($conn, "wine", "year", "minYear", ""); ?> To 
-      <?php selectDistinct($conn, "wine", "year", "maxYear", "1999"); ?>
+      <?php selectDistinct("wine", "year", "minYear", ""); ?> To 
+      <?php selectDistinct("wine", "year", "maxYear", "1999"); ?>
       </td>
       <!-- If an error was returned from the results page
       then display next to fields -->
